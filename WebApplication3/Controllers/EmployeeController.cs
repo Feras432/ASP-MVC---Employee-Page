2d648899ed480113e0e4a40e8db6c6b33fa44068
firas <firasshdad@gmail.com>
2024-04-16 15:20:43 +0300

    Added Details page.

--- WebApplication3/Controllers/EmployeeController.cs
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -5,15 +5,26 @@ namespace WebApplication3.Controllers
 {
     public class EmployeeController : Controller
     {
+
+        List<Employee> employeeList = [
+        new Employee { EmployeeId = 001, FirstName = "Feras",  LastName = "Alshadad", Email = "f@gmail.com", Department = "IT"},
+        new Employee { EmployeeId = 002, FirstName = "Faisal",  LastName = "Alshadad", Email = "f1@gmail.com", Department = "M&A"},
+        new Employee { EmployeeId = 003, FirstName = "Fawaz",  LastName = "Alshadad", Email = "f2@gmail.com", Department = "CS"}
+            ];
         public IActionResult Index()
         {
 
-            var employees = new List<Employee>();
-            employees.Add(new Employee(001, "Feras", "Alshadad", "f@gmail.com", "IT" ));
-            employees.Add(new Employee(002, "Fahad", "Ahmed", "f1@gmail.com", "HR"));
-            employees.Add(new Employee(003, "Fawaz", "Bader", "f2@gmail.com", "A&M"));
-            employees.Add(new Employee(004, "Faisal", "Talal", "f@3gmail.com", "CS"));
 
+            return View(employeeList);
+        }
+
+        public IActionResult Details (int id)
+        {
+            var employees = employeeList.SingleOrDefault(a=> a.EmployeeId == id);
+            if (employees == null)
+            {
+                return RedirectToAction("Index");
+            }
             return View(employees);
         }
     }
--- WebApplication3/Models/Employee.cs
+++ WebApplication3/Models/Employee.cs
@@ -8,15 +8,7 @@
         public string Email { get; set; }
         public string Department { get; set; }
 
-        public Employee(int EmployeeId, string FirstName, string LastName, string Email, string Department)
-        {
-            this.EmployeeId = EmployeeId;
-            this.FirstName = FirstName;
-            this.FirstName = FirstName;
-            this.LastName = LastName;
-            this.Email = Email;
-            this.Department = Department;
-        }
+       
 
     }
 }
