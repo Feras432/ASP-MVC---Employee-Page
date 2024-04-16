19a366e711e45059779b7a5ddd9edb968e2b1186
firas <firasshdad@gmail.com>
2024-04-16 15:03:25 +0300

    Add project files.

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var employees = new List<Employee>();
+            employees.Add(new Employee(001, "Feras", "Alshadad", "f@gmail.com", "IT" ));
+            employees.Add(new Employee(002, "Fahad", "Ahmed", "f1@gmail.com", "HR"));
+            employees.Add(new Employee(003, "Fawaz", "Bader", "f2@gmail.com", "A&M"));
+            employees.Add(new Employee(004, "Faisal", "Talal", "f@3gmail.com", "CS"));
+
+            return View(employees);
+        }
+    }
+}
---
+++ WebApplication3/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+
+        public Employee(int EmployeeId, string FirstName, string LastName, string Email, string Department)
+        {
+            this.EmployeeId = EmployeeId;
+            this.FirstName = FirstName;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Department = Department;
+        }
+
+    }
+}
